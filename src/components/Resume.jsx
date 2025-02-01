eb7d5bfdec7c6754d4cc908415bdb15729f44ead
Ashvin <ashvinjangid10@gmail.com>
2025-02-01 20:16:56 +0530

    display the education data in the resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,5 @@
 export default function Resume(props) {
     const resume = props.resume;
-
     return (
         <div id="resume">
             <h2>Resume</h2>
@@ -9,6 +8,17 @@ export default function Resume(props) {
                 <span>Name: {resume.name}</span><br />
                 <span>Email: {resume.email}</span><br />
                 <span>Phone Number: {resume.phoneNum}</span><br />
+            </div>
+            <div>
+                <h3>Education</h3>
+                {resume.education.map((edu) => (
+                    <div key={edu.schoolName}>
+                        <span>School: {edu.schoolName}</span><br />
+                        <span>Degree: {edu.degree}</span><br />
+                        <span>Start Date: {edu.startDate}</span><br />
+                        <span>End Date: {edu.endDate}</span><br />
+                    </div>
+                ))}
             </div>
         </div>
     )
