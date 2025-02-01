58e27a79a1d94fc6ce7233d4e6aba86e882d9a9b
Ashvin <ashvinjangid10@gmail.com>
2025-02-01 17:02:09 +0530

    refactor and simplify updating the state in generalInfoSection

--- src/components/GeneralInfoSection.jsx
+++ src/components/GeneralInfoSection.jsx
@@ -1,25 +1,24 @@
 export default function GeneralInfoSection({ resume, updateResume }) {
+    function handleInputChange(e) {
+        const { name, value } = e.target
+        updateResume({ ...resume, [name]: value })
+    }
+
     return (
         <div id="general">
             <h3>General</h3>
             <form>
                 <div>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" onChange={
-                        (e) => updateResume({ ...resume, name: e.target.value })
-                    } />
+                    <input type="text" name="name" onChange={handleInputChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email: </label>
-                    <input type="text" name="email" onChange={
-                        (e) => updateResume({ ...resume, email: e.target.value })
-                    } />
+                    <input type="text" name="email" onChange={handleInputChange} />
                 </div>
                 <div>
                     <label htmlFor="phoneNum">Phone Number: </label>
-                    <input type="text" name="phoneNum" onChange={
-                        (e) => updateResume({ ...resume, phoneNum: e.target.value })
-                    } />
+                    <input type="text" name="phoneNum" onChange={handleInputChange} />
                 </div>
             </form>
         </div>
