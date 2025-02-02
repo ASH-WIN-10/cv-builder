49ce11223e723bbca813118955853d7ce28e0614
Ashvin <ashvinjangid10@gmail.com>
2025-02-02 17:48:13 +0530

    make the 'general' section non-collapsible

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -10,16 +10,29 @@ export function Accordion({ title, children }) {
 
     return (
         <div className="accordion-section">
-            <div
-                className={`accordion-header ${expanded ? "expanded" : ""}`}
-                onClick={toggleSection}
-            >
-                <span className="accordion-title">{title}</span>
-                <span className="accordion-icon">+</span>
-            </div>
-            <div className={`accordion-content ${expanded ? "expanded" : ""}`}>
-                {children}
-            </div>
+            {title === "General Information" ?
+                <>
+                    <div className="accordion-header expanded" >
+                        <span className="accordion-title">{title}</span>
+                    </div>
+                    <div className="accordion-content expanded">
+                        {children}
+                    </div>
+                </>
+                :
+                <>
+                    <div
+                        className={`accordion-header ${expanded ? "expanded" : ""}`}
+                        onClick={toggleSection}
+                    >
+                        <span className="accordion-title">{title}</span>
+                        <span className="accordion-icon">+</span>
+                    </div>
+                    <div className={`accordion-content ${expanded ? "expanded" : ""}`}>
+                        {children}
+                    </div>
+                </>
+            }
         </div>
     );
 }
